eba391ec3d79c17f6f743f9dcc0fa86c37651e49
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-01-27 14:55:39 +0000

    add request endpoint that handles any request that are using non existing/invalid paths

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const app = express();
 const {} = require("./controllers/endpoint");
 const endpoints = require("./endpoints.json");
 
+app.use(express.json());
+
 app.get("/api", (req, res) => {
   res.status(200).send({ endpoints });
+});
+
+//middleware error handlers
+
+app.all("*", (req, res) => {
+  res.status(404).send({ error: "Endpoint not found" });
 });
 
 module.exports = app;
