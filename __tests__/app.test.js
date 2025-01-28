f34c89e5ef391ece9c4716c0fd8e8a6700d2493e
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-01-28 13:38:29 +0000

    add test suite for GET /api/articles endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const endpointsJson = require("../endpoints.json");
 const request = require("supertest");
+const jestSorted = require("jest-sorted");
 const app = require("../app.js");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/");
@@ -20,20 +21,12 @@ describe("GET /api", () => {
 });
 
 describe("GET /api/topics", () => {
-  test("200: Response should be stored in an array", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body: { allTopics } }) => {
-        expect(Array.isArray(allTopics)).toBe(true);
-      });
-  });
-
   test("200: Responds with an array with topic objects where it will have the keys 'slug' and 'description' which is of string value", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
       .then(({ body: { allTopics } }) => {
+        expect(Array.isArray(allTopics)).toBe(true);
         allTopics.forEach((topic) => {
           expect(topic).toEqual(
             expect.objectContaining({
@@ -55,7 +48,7 @@ describe("GET /api/topics", () => {
   });
 });
 
-describe("GET *", () => {
+describe("GET all bad URLs", () => {
   test("404: Should respond with 'Endpoint not found', if a request is sending to an invalid/non existing path", () => {
     return request(app)
       .get("/api/iamgroot")
@@ -109,6 +102,32 @@ describe("GET /api/articles/:article_id", () => {
       .then((response) => {
         expect(response.body.error).toBe("Bad Request");
         expect(response.body.msg).toBe("Invalid input syntax for Article ID");
+      });
+  });
+});
+
+describe("GET /api/articles", () => {
+  test("200: Should respond with 200 status when successfully fetching all object articles which should have the expected properties without the body", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { allArticles } }) => {
+        expect(allArticles).toBeSortedBy("created_at", { descending: true }); //checks if the response is sorted by created_at using descending order by default
+        allArticles.forEach((article) => {
+          expect(article).not.toHaveProperty("body"); //to check if key/property is not existing
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+              comment_count: expect.any(String),
+            })
+          );
+        });
       });
   });
 });
