a187b29d9bdecac44334d5de65b6c0cd5db623b3
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-01-29 00:40:06 +0000

    add checkArticleIdExists() utility function

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,3 +1,5 @@
+const db = require("../connection");
+
 exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
@@ -18,5 +20,20 @@ exports.formatComments = (comments, idLookup) => {
       author: created_by,
       ...this.convertTimestampToDate(restOfComment),
     };
+  });
+};
+
+exports.checkArticleIdExists = (article_id) => {
+  let SQL = `SELECT * FROM articles 
+  WHERE article_id = $1`;
+
+  return db.query(SQL, [article_id]).then((result) => {
+    if (result.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        error: "Not found",
+        detail: "Article ID does not exist",
+      });
+    } else return "Category Exists";
   });
 };
