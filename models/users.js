1f6c7fadeac9c1adede80d6a7b9b5258e5ac247b
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-01-31 20:33:49 +0000

    add model function fetchUserByUsername() for users

--- models/users.js
+++ models/users.js
@@ -1,9 +1,22 @@
 const db = require("../db/connection");
 
+const { checkUsernameExists } = require("../db/seeds/utils");
+
 exports.fetchAllUsers = () => {
   const SQL = `SELECT * FROM users`;
 
   return db.query(SQL).then((result) => {
     return result.rows;
+  });
+};
+
+exports.fetchUserByUsername = (username) => {
+  let SQL = `SELECT * FROM users 
+  WHERE username = $1`;
+
+  return checkUsernameExists(username).then(() => {
+    return db.query(SQL, [username]).then((result) => {
+      return result.rows[0];
+    });
   });
 };
