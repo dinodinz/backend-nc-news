fe0fdb1595e54e470b2a1c25825c792c54746821
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-01-31 20:33:03 +0000

    add controller function getUserByUsername() for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,20 @@
-const { fetchAllUsers } = require("../models/users");
+const { fetchAllUsers, fetchUserByUsername } = require("../models/users");
 
 exports.getAllUsers = (req, res, next) => {
   fetchAllUsers()
     .then((allUsers) => {
       res.status(200).send({ allUsers: allUsers });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  fetchUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user: user });
     })
     .catch((err) => {
       next(err);
