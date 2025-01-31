a85459d86b7fb48cfc2063f5cba1ba654c174b49
dinodin <dinodin@dinodins-MacBook-Pro.local>
2025-01-31 17:08:54 +0000

    move all endpoints to their respective router files leaving only /api/ as the main router

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
 const express = require("express");
 const app = express();
-const { getAllTopics } = require("./controllers/topics");
-const { getAllUsers } = require("./controllers/users");
-const {
-  getArticleById,
-  getAllArticles,
-  updateArticleById,
-} = require("./controllers/articles");
-const {
-  getCommentsByArticleId,
-  addCommentByArticleId,
-  deleteCommentById,
-} = require("./controllers/comments");
-const endpoints = require("./endpoints.json");
+const apiRouter = require("./routers/api-router");
+
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.status(200).send({ endpoints });
-});
-
-app.get("/api/topics", getAllTopics);
-
-app.get("/api/articles", getAllArticles);
-
-app.get("/api/users", getAllUsers);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.post("/api/articles/:article_id/comments", addCommentByArticleId);
-
-app.patch("/api/articles/:article_id", updateArticleById);
-
-app.delete("/api/comments/:comment_id", deleteCommentById);
+app.use("/api", apiRouter);
 
 //middleware error handlers
 
